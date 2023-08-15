cebcb4a0ffbffcbabf7c7a22b91f5dbb25c87803
Matiullah Karimi <matiullah@taplane.com>
2023-08-15 17:37:45 -0400

    Add header components

---
+++ resources/js/Components/Common/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="bg-white">
+        <Navbar></Navbar>
+    </header>
+</template>
+
+<script setup>
+import Navbar from "@/Components/Common/Navbar.vue"
+
+</script>
+
+<style></style>
