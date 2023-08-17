345197dc149fa9ec77f79603952713f9ab2fc0f8
Matiullah Karimi <matiullah@taplane.com>
2023-08-16 22:29:48 -0400

    Add search bar component

--- resources/js/Components/Common/Hero.vue
+++ resources/js/Components/Common/Hero.vue
@@ -1,22 +1,27 @@
+
+<script setup>
+import SearchBar from '@/Components/Common/SearchBar.vue';
+</script>
+
 <template>
-    <div class="bg-white bg-center" style="background-image:url('/images/hero.jpg')">
-        <div class="relative isolate px-6 pt-14 lg:px-8 backdrop-brightness-50">
-            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+    <div class="bg-white bg-cover bg-center" style="background-image:url('/images/hero1.jpg')">
+        <div class="relative isolate px-6 pt-12 lg:px-8 backdrop-brightness-50">
+            <div class="mx-auto max-w-2xl py-12">
                 <div class="hidden sm:mb-8 sm:flex sm:justify-center">
                     <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                         A hub for finding your new property. <a href="#" class="font-semibold text-indigo-600"></a>
                     </div>
                 </div>
                 <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Find your most suitable preconstruction</h1>
+                    <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Your Hub For Your Preconstruction Projects</h1>
                     <p class="mt-6 text-lg leading-8 text-white">A great plateform to buy, sell and rent your properties without any agent or commisions.</p>
                 </div>
+            </div>
+            <div class="w-1/2 mx-auto">
+                <SearchBar />
             </div>
         </div>
     </div>
 </template>
-
-<script setup>
-</script>
 
 <style></style>
---
+++ resources/js/Components/Common/SearchBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-start items-center py-4 relative gap-2">
+        <input class="text-sm leading-none text-left text-gray-600 px-4 py-2 h-12 w-full border rounded border-gray-300 outline-none" type="text" placeholder="Ex: 1 redimount Ave">
+        <button class="px-6 py-2 h-12 bg-green-500 text-white rounded hover:bg-green-600">Search</button>
+    </div>
+</template>
