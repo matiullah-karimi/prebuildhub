2158f0f077a394e3c0ad1e021b69fb5984ab927f
Matiullah Karimi <matiullah.karimi@gmail.com>
2023-08-17 06:56:37 -0400

    Add featured builders component

---
+++ resources/js/Components/Builders/FeaturedBuilders.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BuilderItem from '@/Components/Builders/BuilderItem.vue'
+import { ref } from 'vue'
+
+const builders = ref([
+    { name: 'Builder 1' },
+    { name: 'Builder 2' },
+    { name: 'Builder 3' },
+    { name: 'Builder 4' },
+])
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">Search by Builder</h2>
+        <div class="grid grid-cols-4 gap-4">
+            <BuilderItem  v-for="builder in builders" :builder="builder" :key="builder.name"/>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,11 +2,38 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
 import Hero from '@/Components/Common/Hero.vue';
+import PropertyList from '@/Components/Properties/PropertyList.vue';
+import ListTitle from '@/Components/Properties/ListTitle.vue';
+import FeaturedBuilders from '@/Components/Builders/FeaturedBuilders.vue'
+
+const sections = [
+    {
+        title: 'Featured Projects',
+        description: 'A great plateform to buy, sell and rent your properties without any agent or commisions.'
+    },
+    {
+        title: 'Upcoming Projects',
+        description: 'A great plateform to buy, sell and rent your properties without any agent or commisions.'
+    },
+    {
+        title: 'Sold Out Projects',
+        description: 'A great plateform to buy, sell and rent your properties without any agent or commisions.'
+    },
+]
 </script>
 
 <template>
-    <AppLayout>
+    <AppLayout title="Home">
         <Hero />
+        <div class="container px-16 max-w-7xl">
+            <div class="mt-12" v-for="section in sections" :key="section.title">
+                <ListTitle :title="section.title" :description="section.description"/>
+                <PropertyList />
+            </div>
+            <div class="mt-12">
+                <FeaturedBuilders />
+            </div>
+        </div>
     </AppLayout>
 </template>
 
