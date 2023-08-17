df9597809d4e3222eb6b046e4f5a121a5ce7e742
Matiullah Karimi <matiullah@taplane.com>
2023-08-16 22:00:05 -0400

    Add property list and item components

---
+++ resources/js/Components/Properties/ListTitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="grid grid-cols-1 pb-8 text-center">
+        <h3 class="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">Featured Properties</h3>
+
+        <p class="text-slate-400 max-w-xl mx-auto">A great plateform to buy, sell and rent your properties without any agent or commisions.</p>
+    </div>
+</template>
---
+++ resources/js/Components/Properties/PropertyList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-8 gap-[30px]">
+    <PropertyItem />
+    <PropertyItem />
+    <PropertyItem />
+  </div>
+</template>
+
+<script setup>
+import PropertyItem from '@/Components/Properties/PropertyItem.vue'
+</script>
+
+<style>
+
+</style>
