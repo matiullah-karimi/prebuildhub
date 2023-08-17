653ae442ac6ac4cb0a725209f45fff441e8f5e17
Matiullah Karimi <matiullah@taplane.com>
2023-08-16 22:30:07 -0400

    Add dynamic list title

--- resources/js/Components/Properties/ListTitle.vue
+++ resources/js/Components/Properties/ListTitle.vue
@@ -1,7 +1,15 @@
+<script setup>
+defineProps({
+    title: String,
+    description: String
+})
+</script>
+
+
 <template>
     <div class="grid grid-cols-1 pb-8 text-center">
-        <h3 class="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">Featured Properties</h3>
+        <h3 class="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">{{ title }}</h3>
 
-        <p class="text-slate-400 max-w-xl mx-auto">A great plateform to buy, sell and rent your properties without any agent or commisions.</p>
+        <p class="text-slate-400 max-w-xl mx-auto">{{ description }}</p>
     </div>
 </template>
