942196e020ce3861e5900f182ebf2a401cd2fcda
Matiullah Karimi <matiullah.karimi@gmail.com>
2023-09-02 23:36:12 -0400

    Production build

--- public/build/assets/InputLabel-364483c7.js
+++
@@ -1,1 +0,0 @@
-import{o as e,e as t,t as o,r as n}from"./app-118bf072.js";const l={class:"block font-semibold text-gray-700 dark:text-gray-300"},r={key:0},c={key:1},p={__name:"InputLabel",props:{value:String},setup(a){return(s,_)=>(e(),t("label",l,[a.value?(e(),t("span",r,o(a.value),1)):(e(),t("span",c,[n(s.$slots,"default")]))]))}};export{p as _};
