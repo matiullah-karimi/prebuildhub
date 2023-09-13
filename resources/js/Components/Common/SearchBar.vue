5f5bb8c5cd881d12e42249e7b33590af71a623ec
Matiullah Karimi <matiullah.karimi@gmail.com>
2023-09-12 22:06:37 -0400

    Replace simple input with TextInput component

--- resources/js/Components/Common/SearchBar.vue
+++ resources/js/Components/Common/SearchBar.vue
@@ -1,10 +1,11 @@
 <script setup>
     import PrimaryButton from '@/Components/PrimaryButton.vue';
+    import TextInput from '@/Components/TextInput.vue';
 </script>
 
 <template>
     <div class="flex justify-start items-center py-4 relative gap-2">
-        <input class="text-sm leading-none text-left text-gray-600 px-4 h-14 w-full border rounded-lg border-gray-300 outline-none" type="text" placeholder="Ex: 2104 Charmaine Lane Amarillo">
+        <TextInput class="px-4 h-14 w-full" type="text" placeholder="Ex: 2104 Charmaine Lane Amarillo" />
         <PrimaryButton class="h-14">Search</PrimaryButton>
     </div>
 </template>
