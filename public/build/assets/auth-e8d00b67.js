98c71f16506fc54a75a3e9659ec6d67e8457ad66
Matiullah Karimi <matiullah.karimi@gmail.com>
2023-10-05 21:05:48 -0400

    Production build

--- public/build/assets/auth-e8d00b67.js
+++
@@ -1,1 +0,0 @@
-import{K as u}from"./app-9021469d.js";const o=()=>{var r;const s=u();return{isLoggedIn:s.props.auth.user!==null,isAdmin:((r=s.props.auth.user)==null?void 0:r.is_admin)??!1}};export{o as u};
